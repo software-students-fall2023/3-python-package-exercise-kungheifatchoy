bc6d3330dbc718e7a77ea592d54a7979bfca811b
ni2050 <ni2050@nyu.edu>
2023-11-06 22:31:30 -0500

    readString passes tests

--- src/pyquadratic/test_pyquadratic.py
+++ src/pyquadratic/test_pyquadratic.py
@@ -12,61 +12,49 @@ def test__readString_parsing_success():
     expected = [1,1,1]
     assert actual == expected, f"CASE 1: Expected Return: {expected}; Actual Return: {actual}"  
 
-    actual = pyquadratic._readString("x + 1")
-    expected = [0,1,1]
-    assert actual == expected, f"CASE 2: Expected Return: {expected}; Actual Return: {actual}"  
-
     actual = pyquadratic._readString("x^2 + x")
     expected = [1,1,0]
-    assert actual == expected, f"CASE 3: Expected Return: {expected}; Actual Return: {actual}"  
+    assert actual == expected, f"CASE 2: Expected Return: {expected}; Actual Return: {actual}"  
 
     actual = pyquadratic._readString("x^2 + 1")
     expected = [1,0,1]
-    assert actual == expected, f"CASE 4: Expected Return: {expected}; Actual Return: {actual}" 
-
-    actual = pyquadratic._readString("1")
-    expected = [0,0,1]
-    assert actual == expected, f"CASE 5: Expected Return: {expected}; Actual Return: {actual}" 
-
-    actual = pyquadratic._readString("x")
-    expected = [0,1,0]
-    assert actual == expected, f"CASE 6: Expected Return: {expected}; Actual Return: {actual}" 
+    assert actual == expected, f"CASE 3: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("x^2")
     expected = [1,0,0]
-    assert actual == expected, f"CASE 7: Expected Return: {expected}; Actual Return: {actual}" 
+    assert actual == expected, f"CASE 4: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("x^2 + 1")
     expected = [1,0,1]
-    assert actual == expected, f"CASE 8: Expected Return: {expected}; Actual Return: {actual}" 
+    assert actual == expected, f"CASE 5: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("1.1x^2 + 1.1x + 1.1")
     expected = [1.1,1.1,1.1]
-    assert actual == expected, f"CASE 9: Expected Return: {expected}; Actual Return: {actual}" 
+    assert actual == expected, f"CASE 6: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("1.1x^2 + 1x + 1.1")
     expected = [1.1,1,1.1]
-    assert actual == expected, f"CASE 10: Expected Return: {expected}; Actual Return: {actual}" 
+    assert actual == expected, f"CASE 7: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("1x^2+1x+1")
-    expected = [1.1,1,1.1]
-    assert actual == expected, f"CASE 11: Expected Return: {expected}; Actual Return: {actual}" 
+    expected = [1,1,1]
+    assert actual == expected, f"CASE 8: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("1x^2+1x+ 1")
-    expected = [1.1,1,1.1]
-    assert actual == expected, f"CASE 12: Expected Return: {expected}; Actual Return: {actual}" 
+    expected = [1,1,1]
+    assert actual == expected, f"CASE 9: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("1x^2 + 1x+1")
-    expected = [1.1,1,1.1]
-    assert actual == expected, f"CASE 13: Expected Return: {expected}; Actual Return: {actual}" 
+    expected = [1,1,1]
+    assert actual == expected, f"CASE 10: Expected Return: {expected}; Actual Return: {actual}" 
 
     actual = pyquadratic._readString("-x^2 - x - 1")
     expected = [-1,-1,-1]
-    assert actual == expected, f"CASE 1: Expected Return: {expected}; Actual Return: {actual}"  
+    assert actual == expected, f"CASE 11: Expected Return: {expected}; Actual Return: {actual}"  
     
     actual = pyquadratic._readString("-1x^2 - 1x - 1")
     expected = [-1,-1,-1]
-    assert actual == expected, f"CASE 1: Expected Return: {expected}; Actual Return: {actual}"  
+    assert actual == expected, f"CASE 12: Expected Return: {expected}; Actual Return: {actual}"  
 
 #CASE 11 - 
 def test__readString_parsing_fail():
@@ -87,6 +75,21 @@ def test__readString_parsing_fail():
 
     with pytest.raises(ValueError) as msg:
         pyquadratic._readString("x+x")
+    error_msg = str(msg.value)
+    assert "Error: Invalid Input, Try Again" == error_msg, f"Expected Msg: Error: No Invalid Input; Actual Msg: {error_msg}"
+    
+    with pytest.raises(ValueError) as msg:
+        pyquadratic._readString("x")
+    error_msg = str(msg.value)
+    assert "Error: Invalid Input, Try Again" == error_msg, f"Expected Msg: Error: No Invalid Input; Actual Msg: {error_msg}"
+
+    with pytest.raises(ValueError) as msg:
+        pyquadratic._readString("1")
+    error_msg = str(msg.value)
+    assert "Error: Invalid Input, Try Again" == error_msg, f"Expected Msg: Error: No Invalid Input; Actual Msg: {error_msg}"
+
+    with pytest.raises(ValueError) as msg:
+        pyquadratic._readString("0x^2+3x+7")
     error_msg = str(msg.value)
     assert "Error: Invalid Input, Try Again" == error_msg, f"Expected Msg: Error: No Invalid Input; Actual Msg: {error_msg}"
     
@@ -148,7 +151,8 @@ def test_to_vertex_form_various_coefficients():
         ("x^2 + x", "(x + 0.5)^2 - 0.25", "no constant term"),
         ("1000x^2 + 2000x + 1000", "1000.0(x + 1.0)^2", "large coefficients"),
         ("x^2 - 4", "x^2 - 4", "no x term"),
-        ("3x^2 + 3x + 1", "3.0(x + 0.5)^2 + 0.25", "positive coefficients")
+        ("3x^2 + 3x + 1", "3.0(x + 0.5)^2 + 0.25", "positive coefficients"),
+        
     ]
 
     for expression, expected, msg in test_cases:
