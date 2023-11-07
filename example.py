0790e2d6a6a680fb950b603e64ca445db298a5b7
ni2050 <ni2050@nyu.edu>
2023-11-07 18:13:15 -0500

    decimal fix realSol., readme update(incomplete)

---
+++ example.py
@@ -0,0 +1,12 @@
+from src.pyquadratic.pyquadratic import *
+
+eq = "2x^2 -4x +2"
+print("Original Equation: " + eq)
+eq = simplifyQuadratic(eq)
+print("Simplified Equation: " + eq)
+vertex_form = toVertexForm(eq)
+factored_form = toFactoredForm(eq)
+print("Vertex Form: " + vertex_form)
+print("Factored Form: " + factored_form)
+real_solution = realSolution(eq)
+print("Solution: x_1 = " + str(real_solution[0]) + "; x_2 = " + str(real_solution[1]))
